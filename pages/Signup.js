070142227ddece02f32c018b1992212eb75bd9b9
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-05-04 07:34:19 +0530

    commit

--- pages/Signup.js
+++ pages/Signup.js
@@ -1,8 +1,129 @@
 import React from 'react'
 
 const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
-    <div>Signup</div>
+    <div className="mt-1 py-20 ml-4">
+      <div className="md:col-span-1">
+        <div className="p-4 ">
+          <h3 className="text-lg font-medium leading-6 text-gray-900">Personal Information</h3>
+          <p className="mt-1 py-2 text-sm text-gray-600">Use a permanent address where you can receive Orders.</p>
+        </div>
+      </div>
+      <div className="m-5  ">
+        <form action={"/"} className="bg-pink-300 rounded-lg" method="POST">
+          <div className="shadow  overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5  sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                    First name
+                  </label>
+                  <input
+                    type="text"
+                    name="first-name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                    Last name
+                  </label>
+                  <input
+                    type="text"
+                    name="last-name"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-4">
+                  <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    name="email-address"
+                    id="email-address"
+                    autoComplete="email"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+
+
+                <div className="col-span-6">
+                  <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+                    Street address
+                  </label>
+                  <textarea
+                    type="text"
+                    name="street-address"
+                    id="street-address"
+                    autoComplete="street-address"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    autoComplete="address-level2"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label htmlFor="region" className="block text-sm font-medium text-gray-700">
+                    State / Province
+                  </label>
+                  <input
+                    type="text"
+                    name="region"
+                    id="region"
+                    autoComplete="address-level1"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label htmlFor="postal-code" className="block text-sm font-medium text-gray-700">
+                    ZIP / Postal code
+                  </label>
+                  <input
+                    type="text"
+                    name="postal-code"
+                    id="postal-code"
+                    autoComplete="postal-code"
+                    className="mt-1 py-2 focus:ring-pink-500 focus:border-pink-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="px-4 py-3  text-right sm:px-6">
+              <button
+                type="submit"
+                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-500"
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
   )
 }
 
